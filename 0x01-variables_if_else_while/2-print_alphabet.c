b9d1321a62dfcd7df584f77ff3495d393f083ad9
Emmanuel <emmywylie23@gmail.com>
2022-06-08 07:39:27 -0700

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -13,11 +13,11 @@ int main(void)
 	char letter;
 
 	srand(time(0));
-	n = rand() -RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 
-	     for (letter = 'a'; letter <= 'z'; letter++)
-		     putchar(letter);
+	for (letter ='a'; letter <='z'; letter++)
+		putchar(letter);
 
 	putchar("\n");
 	return (0);
