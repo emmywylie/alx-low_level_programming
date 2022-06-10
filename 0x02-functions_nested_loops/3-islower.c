7909a0c69565a2b680a6b59bc359c13d49cf36d7
Emmanuel <emmywylie23@gmail.com>
2022-06-10 08:25:26 -0700

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * ialower - checks for lowercase character.
+ * islower - checks for lowercase character.
  * @c: The character to check
  * Return: 1 if c is lowercase, 0 otherwise
  */
