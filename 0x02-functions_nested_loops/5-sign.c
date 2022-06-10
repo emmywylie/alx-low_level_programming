ac2d7ce423c82bc336774a1a38072ea6fa8a030f
Emmanuel <emmywylie23@gmail.com>
2022-06-10 12:23:53 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,7 +12,7 @@ int print_sign(int n)
 
 {
 	if (n > 0)
-	
+
 	{
 		_putchar('+');
 		return (1);
@@ -26,7 +26,7 @@ int print_sign(int n)
 
 	}
 
-	else if (n <0) 
+	else if (n < 0) 
 	{
 		_putchar('-');
 	}
