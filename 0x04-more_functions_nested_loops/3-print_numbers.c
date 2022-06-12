8ed6b8dff7f928592b6ec7e1a75a81fe2484cbb2
Emmanuel <emmywylie23@gmail.com>
2022-06-11 18:16:05 -0700

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints numbers, from 0-9
+ *
+ * return: void
+ */
+
+void print_numbers(void)
+
+{
+	char c;
+	
+	for (c = '0'; c <= '9'; c++)
+
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+
+}	
