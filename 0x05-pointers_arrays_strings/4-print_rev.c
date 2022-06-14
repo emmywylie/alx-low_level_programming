0e893173df1b7739825e7026af67740d21558746
Emmanuel <emmywylie23@gmail.com>
2022-06-14 05:33:49 -0700

    4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,18 +9,8 @@
 
 void print_rev(char *s)
 {
-	int i = 0, len = 0;
-	char tmp;
-
-	while (s[i++])
-		len++;
-	for (i = len -1; i >= len / 2; i--)
-
+	while (*s)
 	{
-		tmp = s[i];
-		s[i] = s[len - i - 1];
-		s[len - i - 1] = tmp;
-
+		putchar(*s--);
 	}
-
 }
